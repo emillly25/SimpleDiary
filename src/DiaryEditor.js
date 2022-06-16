dd390e1b24854da5b286b58d9e5a312773aa09ca
sehee <seeeh0128@gmail.com>
2022-06-16 22:00:54 +0900

    state 2개 하나로 합치기

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,23 +1,35 @@
 import { useState } from 'react'
 
 const DiaryEditor = () => {
-
+    const [state, setState] = useState({
+        author:'',
+        content:'',
+    })
     const [author, setAuthor] = useState('')
     const [content, setContent] = useState('')
     return (
         <div className='DiaryEditor'>
             <h2>Log toDay</h2>
             <div>
-                <input type="text" value={author} onChange={(e)=>{
-                    setAuthor(e.target.value)
+                <input type="text" value={state.author} onChange={(e)=>{
+                    setState(cur=>{
+                        const newState = {...cur}
+                        newState.author =e.target.value
+                        return newState
+                    })
                 }}></input>
             </div>
             <div>
-                <textarea value={content} onChange={(e)=>{
-                    setContent(e.target.value)
+                <textarea value={state.content} onChange={(e)=>{
+                    setState(cur=>{
+                        const newState = {...cur}
+                        newState.content = e.target.value
+                        return newState
+                    })
                 }}></textarea>
             </div>
-
+                {state.author}
+                {state.content}
         </div>
 
     )
